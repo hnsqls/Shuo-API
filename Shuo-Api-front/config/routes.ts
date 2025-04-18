f60710f916e8c7523d4f44c262e4e8701feda563
hnsqls <2661108038@qq.com>
2025-04-18 15:26:05 +0800

    doc

--- Shuo-Api-front/config/routes.ts
+++ Shuo-Api-front/config/routes.ts
@@ -12,10 +12,11 @@ export default [
     access: 'canAdmin',
     routes: [
       { path: '/admin', redirect: '/admin/sub-page' },
-      { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
+      { path: '/admin/sub-page', name: '接口管理', component: './TableList' },
     ],
   },
-  { name: '接口广场', icon: 'table', path: '/list', component: './TableList' },
+  { name: '接口广场', icon: 'table', path: '/interfacelist', component: './InterfaceInfo' },
+  { path: '/interfaceinfo/:id',  component: './InterfaceInfoDetail' }, // 新增详情页路由
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
